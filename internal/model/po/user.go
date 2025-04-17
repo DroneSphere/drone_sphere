9c9c7aaac9692640521b2cd06631fef311130cd1
GuoSurui <thuray.email@gmail.com>
2025-04-17 13:17:05 +0800

    fix: 修复用户模型中的时间字段类型，使用 time.Time 替代 int64

--- internal/model/po/user.go
+++ internal/model/po/user.go
@@ -1,15 +1,16 @@
 package po
 
+import "time"
+
 type User struct {
-	ID          uint   `json:"user_id" gorm:"primaryKey;column:user_id"`
-	CreatedTune int64  `json:"created_time" gorm:"autoCreateTime"`
-	UpdatedTune int64  `json:"updated_time" gorm:"autoUpdateTime"`
-	DeletedTune int64  `json:"deleted_time" gorm:"autoDeleteTime"`
-	State       int    `json:"state" gorm:"default:0"` // -1: deleted, 0: active
-	Username    string `json:"username" gorm:"unique"`
-	Email       string `json:"email"`
-	Avatar      string `json:"avatar"`
-	Password    string `json:"password"`
+	ID          uint      `json:"user_id" gorm:"primaryKey;column:user_id"`
+	CreatedTime time.Time `json:"created_time" gorm:"autoCreateTime"`
+	UpdatedTime time.Time `json:"updated_time" gorm:"autoUpdateTime"`
+	State       int       `json:"state" gorm:"default:0"` // -1: deleted, 0: active
+	Username    string    `json:"username" gorm:"unique"`
+	Email       string    `json:"email"`
+	Avatar      string    `json:"avatar"`
+	Password    string    `json:"password"`
 }
 
 // TableName 指定 User 表名为 tb_users
